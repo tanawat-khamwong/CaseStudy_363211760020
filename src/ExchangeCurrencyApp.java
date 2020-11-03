93ac850240d22e5d6cd548dbccf4b6ec82165917
bank <tanawat.k@rmutsvmail.com>
2020-11-03 18:26:19 +0700

    case study

---
+++ src/ExchangeCurrencyApp.java
@@ -0,0 +1,9 @@
+public class ExchangeCurrencyApp {
+    public static void main (String[] args){
+        ExchangeAPI ex = new ExchangeAPI();
+
+        // test connection with THB
+        if (ex.getConnection("THB"));
+            System .out.println(ex.getResult());
+    }
+}
